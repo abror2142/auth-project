1a648998464fe6bad79fab8546180336828ebb49
Abrorbek Ismoilov <abror2142@gmail.com>
2025-01-27 01:11:44 +0500

    Fixing bug: rendering two notifications

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-set -e  # Exit immediately if a command exits with a non-zero status
+set -e
 
 echo "Starting initialization..."
 
-# Wait for the database to be ready (optional)
 if [ "$WAIT_FOR_DB" = "true" ]; then
   echo "Waiting for database connection..."
   until nc -z -v -w30 db 5432; do
@@ -13,11 +12,8 @@ if [ "$WAIT_FOR_DB" = "true" ]; then
   echo "Database is ready!"
 fi
 
-# Run Doctrine migrations
-echo "Running Doctrine migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 echo "Initialization complete. Starting php-fpm..."
 
-# Hand over control to CMD specified in Dockerfile
 exec "$@"
